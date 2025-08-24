6cb267b9e6eabf0d0d2e178a85c67555120a9f8d
irfanmmm <koyarabigh890@gmail.com>
2025-08-24 07:37:34 -0700

    first commit

---
+++ Components/Employee/EmployeeStackNavigator.js
@@ -0,0 +1,39 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { Image, Text, View } from "react-native";
+import Scan from "./Screens/Scan";
+import Status from "./Screens/Status";
+import Avatar from "../Assets/icon.png";
+function LogoTitle() {
+  return (
+    <View
+      style={{
+        minHeight: 60,
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 22, fontWeight: "800", paddingLeft: 20 }}>
+        AttendEase
+      </Text>
+    </View>
+  );
+}
+
+const EmployeeStackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName="Scan"
+      screenOptions={{ headerShown: true }}
+    >
+      <Stack.Group>
+        <Stack.Screen name="Scan" component={Scan} />
+        <Stack.Screen name="Status" component={Status} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
+
+export default EmployeeStackNavigator;
